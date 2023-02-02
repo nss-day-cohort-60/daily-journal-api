0b489acae2d1a85e5f9ea6f0b106f89f7e21ca5e
Grace Boaz <anna.g.boaz@gmail.com>
2023-02-02 10:20:46 -0600

    create_tag adds a tag to the databse

---
+++ views/tags_requests.py
@@ -0,0 +1,19 @@
+import sqlite3
+import json
+from models import Tag 
+
+def create_tag(new_tag):
+    """ creates a new tag and adds it to the database """
+    with sqlite3.connect("./journal.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Tags 
+            (subject)
+        VALUES
+            (?)
+        """, (new_tag['subject'], ))
+
+        id = db_cursor.lastrowid
+        new_tag['id'] = id
+    return new_tag
