602e6b27cf5fef2937010d16462f3015717ede62
kimberly burton <kj.burton03@gmail.com>
2023-02-03 11:02:36 -0600

    entries edit

--- views/__init__.py
+++ views/__init__.py
@@ -3,5 +3,5 @@ from .entries_requests import create_entry
 from .entries_requests import delete_entry
 from .entry_tags_requests import delete_entry_tag_with_entryid
 from .moods_requests import get_all_moods
-from .entries_requests import get_single_entry, get_all_entries
+from .entries_requests import get_single_entry, get_all_entries, update_entry
 from .users_requests import update_user
